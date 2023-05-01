fcd9a2dd5b307e6a5d7e44be221a121f8b1ca4ed
Britodfbr <britodfbr@gmail.com>
2023-05-01 08:57:43 -0300

    refactor: Fatoramento de rotas e conexões de dados

---
+++ incolume/py/fastapi/crud2/data_base/connections.py
@@ -0,0 +1,34 @@
+from typing import List
+from uuid import uuid4
+from models import Gender, Role, User
+
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="John",
+        last_name="Doe",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Jane",
+        last_name="Doe",
+        gender=Gender.female,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="James",
+        last_name="Gabriel",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Eunit",
+        last_name="Eunit",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user],
+    ),
+]
